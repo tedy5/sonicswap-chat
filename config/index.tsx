b324f064367f573cffa997c7071251e974792eaf
tedy <tedy69@gmail.com>
2025-02-01 20:54:03 +0100

    Add multiple chains for deBridge compatibility

--- config/index.tsx
+++ config/index.tsx
@@ -1,9 +1,39 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sonic } from 'wagmi/chains';
+import {
+  mainnet,
+  bsc,
+  polygon,
+  arbitrum,
+  avalanche,
+  fantom,
+  linea,
+  optimism,
+  base,
+  gnosis,
+  metis,
+  sonic,
+  cronos,
+  neonMainnet
+} from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'SonicSwap AI Chat',
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  chains: [sonic],
+  chains: [
+    mainnet,
+    bsc,
+    polygon,
+    arbitrum,
+    avalanche,
+    fantom,
+    linea,
+    optimism,
+    base,
+    gnosis,
+    metis,
+    sonic,
+    cronos,
+    neonMainnet
+  ],
   ssr: true,
 });
