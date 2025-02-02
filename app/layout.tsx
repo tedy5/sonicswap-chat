a6293c97f5b88b99d4e3ed964f30ebba882c2d66
tedy <tedy69@gmail.com>
2025-02-02 12:16:43 +0100

    Add Sonner toast notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Header } from '@/components/Header';
 import { Providers } from '@/providers';
 import '@rainbow-me/rainbowkit/styles.css';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -27,6 +28,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} dark font-sans`} suppressHydrationWarning={true}>
       <body className={`${inter.className} font-sans`}>
+        <Toaster position="top-center" richColors />
         <div className="relative flex min-h-screen flex-col">
           <div className="absolute bottom-0 left-0 right-0 top-0 -z-20 bg-[#0a0b1e]" />
           <Providers>
